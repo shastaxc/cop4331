2fdee9b0ae02342077a012cb49b2d7185a33da78
shastaxc <djordan2@knights.ucf.edu>
2014-06-21 23:48:45 -0400

    Removed Toast code from GameInstance
    Going to be using a custom popup window for settings, toast would be
    redundant.

--- bentd/src/cop4331/cloud9001/bentd/GameInstance.java
+++ bentd/src/cop4331/cloud9001/bentd/GameInstance.java
@@ -2,18 +2,13 @@ package cop4331.cloud9001.bentd;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class GameInstance extends Activity {
 	protected static String game_state;
-	private Toast pause_toast;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +30,6 @@ public class GameInstance extends Activity {
 		Button settings_btn = (Button)findViewById(R.id.settings_btn);
 		settings_btn.setOnClickListener(globalOnClickListener);
 		
-        pause_toast = new Toast(getApplicationContext());
-		
 		game_state = "active";
 	}
 	
@@ -57,24 +50,6 @@ public class GameInstance extends Activity {
     //TODO
     private static void pauseBtnClick(){
     	game_state = "paused";
-		//=============================================
-		//Displays toast with pause text
-		
-		LayoutInflater inflater = getLayoutInflater();
-		//Assign the custom layout to view
-		//Parameter 1 - Custom layout XML
-		//Parameter 2 - Custom layout ID present in linearlayout tag of XML
-		View layout = inflater.inflate(R.layout.toast_custom_layout,
-		            (ViewGroup) findViewById(R.id.toast_layout));
-		//Return the application context
-		TextView text = (TextView) layout.findViewById(R.id.toast_textview);
-		text.setText("Paused");
-		pause_toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-		pause_toast.setDuration(Toast.LENGTH_SHORT);
-		//Set the custom layout to Toast
-		pause_toast.setView(layout);
-		//Display toast
-		pause_toast.show();
     }
     
     private static void settingsBtnClick(){
