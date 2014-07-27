2dd26d557db05c5dd1e876a6b986461069fa3913
shastaxc <djordan2@knights.ucf.edu>
2014-07-27 14:44:16 -0400

    Added foundation for high score fragment
    Need to add reading from file to populate listview. Will sort high
    scores prior to writing them to file so that check will not be necessary
    for displaying in the listview.

---
+++ bentd/src/cop4331/cloud9001/bentd/Score.java
@@ -0,0 +1,18 @@
+package cop4331.cloud9001.bentd;
+
+public class Score {
+	private String player;
+	private String score;
+	
+	public Score(String p, String s){
+		this.player = p;
+		this.score = s;
+	}
+	
+	public String getPlayer(){
+		return this.player;
+	}
+	public String getScore(){
+		return this.score;
+	}
+}
